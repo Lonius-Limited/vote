af7574520cbcdc6fb13b82f48649761e684faa9a
Thomas Mwogi <mwogin@gmail.com>
2021-03-20 15:56:03 +0000

    Initial commit

---
+++ vote/vote/doctype/electoral_district/electoral_district.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, team_paperless and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Electoral District', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ vote/vote/doctype/electoral_district/electoral_district.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, team_paperless and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class ElectoralDistrict(NestedSet):
+	pass
