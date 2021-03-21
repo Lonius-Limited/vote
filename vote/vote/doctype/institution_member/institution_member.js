6d4648f75684fbd66f52ea6ea93017b58d8d4a55
Salim Mwaura <dsmwaura@gmail.com>
2021-03-21 21:12:57 +0000

    tbd

--- vote/vote/doctype/institution_member/institution_member.js
+++ vote/vote/doctype/institution_member/institution_member.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Institution Member', {
-	// refresh: function(frm) {
-
-	// }
+	onload_post_render: function(frm) {
+		console.log("Applying filters")
+		frm.set_query('electoral_district',()=>{
+			return {
+				"filters":{
+					'is_group': 0
+				}			
+			}
+		})
+	}
 });
