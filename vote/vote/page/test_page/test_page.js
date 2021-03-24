49d277e6c6e34616c43ba53d8ffca14a8e9924d4
Thomas Mwogi <thomasmwogi@mtrh.go.ke>
2021-03-24 19:55:13 +0000

    initial vue page template and build up

--- vote/vote/page/test_page/test_page.js
+++ vote/vote/page/test_page/test_page.js
@@ -1,7 +1,11 @@
 frappe.pages['test-page'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Test Page',
-		single_column: true
-	});
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Test Page',
+        single_column: true
+    });
+
+    page.$export_tool = new frappe.testpage.TestChart(page)
+        //page.$export_tool = new frappe.nursing.ExportTool(page);
+    console.log("testpage: " + page);
 }
