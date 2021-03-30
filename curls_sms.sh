f53f06e23c126f8bfccfbcaf063b687acd550716
Salim Mwaura <dsmwaura@gmail.com>
2021-03-30 15:07:29 +0000

    FIxes and features

---
+++ curls_sms.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+    https://api.sandbox.africastalking.com/version1/messaging \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'apiKey: f8d559ca4607bdf6fee2a988b158b5857ed26f88e6fb29cdcdf730fb8c830c53' \
+    -d 'username=salim1990&to=%2B254722810063,%2B254719415812&message=Hello%20World!'
