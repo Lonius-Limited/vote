1edff19a17667e8acbed6370aa97df1cff153beb
Thomas Mwogi <thomasmwogi@mtrh.go.ke>
2021-04-25 14:25:21 +0000

    Functionality for sandbox

--- vote/vote/doctype/institution_member_sandbox/institution_member_sandbox.py
+++ vote/vote/doctype/institution_member_sandbox/institution_member_sandbox.py
@@ -20,15 +20,10 @@ class InstitutionMemberSandbox(Document):
 		try:
 			if self.create_a_new_record: frappe.throw("Not Implemented yet!")
 			master_doc = frappe.get_doc("Institution Member", self.link_with_document)
-			master_doc.set("member_id",self.member_id)
-			master_doc.set("id_number",self.id_number)
-			master_doc.set("board_number",self.board_number)
-			master_doc.set("electoral_district",self.electoral_district)
-			master_doc.set("surname",self.surname)
-			master_doc.set("other_names",self.other_names)
 			master_doc.set("cell_number",self.cell_number)
 			master_doc.set("email_address",self.email_address)
-			master_doc.save(ignore_permissions=True)			
+			master_doc.save(ignore_permissions=True)	
+			master_doc.db_set("board_number",self.board_number)		
 			return master_doc.name
 		except Exception as e:
 			frappe.throw(f"{e}")
