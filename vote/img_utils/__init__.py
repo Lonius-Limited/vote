2748c4e07f76f341c3401d559a00d365e3e6ba1f
Thomas Mwogi <thomasmwogi@mtrh.go.ke>
2021-05-03 22:46:04 +0300

    Some bunch of changes

--- vote/img_utils/__init__.py
+++ vote/img_utils/__init__.py
@@ -4,8 +4,14 @@ import frappe
 @frappe.whitelist(allow_guest=True)
 def get_headshot(id=''):
     # Contex is Voter ID 
-    return frappe.get_doc("Institution Member", id).get("image")
+    base_url = frappe.get_doc("Institution Member", id).get("image") or ''
+    if 'http' in base_url:
+        return base_url
+    return frappe.utils.get_url(str(base_url))
 @frappe.whitelist(allow_guest=True)
 def get_party_symbol(id=''):
     # Contex is Political Party Name 
-    return frappe.get_doc("Political Party", id).get("party_symbol")
+    base_url = frappe.get_doc("Political Party", id).get("party_symbol") or ''
+    if 'http' in base_url:
+        return base_url
+    return frappe.utils.get_url(str(base_url))
