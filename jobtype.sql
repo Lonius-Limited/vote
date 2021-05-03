f6abdb12f20e656981f029038b5964df7d87d7fc
Thomas Mwogi <thomasmwogi@mtrh.go.ke>
2021-05-03 17:29:07 +0300

    Selective scheduled bulk sms

--- jobtype.sql
+++ jobtype.sql
@@ -1,3 +1,4 @@
+DELETE FROM  `tabScheduled Job Type` WHERE name ='bulk_sms_switch';
 
 INSERT INTO `tabScheduled Job Type` (`name`,method,frequency,cron_format,create_log)
-VALUES('election_switch','vote.utils.election_details.election_status_switch','Cron','* * * * * *',1);
+VALUES('bulk_sms_switch','vote.vote.doctype.bulk_messaging.handler.handle_sms_cron','Cron','* * * * * *',1);
