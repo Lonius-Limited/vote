9a18f6b4a522f7bd82e19cbfd6d0f72d1b11abcf
Thomas Mwogi <mwogin@gmail.com>
2023-07-31 22:02:22 +0000

    Tweaks to header and landing

--- evote/src/Index.jsx
+++ evote/src/Index.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Row } from "antd";
+import { Button, Col, Row, Card } from "antd";
 import React from "react";
 import electionSVG from "../public/undraw_election_day_w842.svg";
 import { Carousel } from "antd";
@@ -28,19 +28,29 @@ const Index = () => {
 };
 const Carousel123 = () => {
   const contentStyle = {
-    height: "260px",
+    // height: "260px",
     color: "#fff",
-    lineHeight: "260px",
+    // lineHeight: "260px",
     textAlign: "center",
     background: "#8F68EF",
+    padding: "54px"
   };
   return (
-    <Carousel autoplay>
+    <Carousel autoplay motionDurationSlow ='5s'>
       <div>
-        <h3 style={contentStyle}>Login with your VoterID and OTP</h3>
+        {/* <h3 style={contentStyle}>Login with your PF Number and OTP - In order to vote, you are required to securely login to the platform. Please use your employer Personal File (PF). We will send you a One Time Password (OTP) to your phone number in our records.</h3> */}
+        <Card title="Login with your PF Number and OTP" bordered={false} style={contentStyle}>
+          <p>In order to vote, you are required to securely login to the platform. Please use your employer Personal File (PF). We will send you a One Time Password (OTP) to your phone number in our records.</p>
+          <p></p>
+          <p></p>
+        </Card>
       </div>
       <div>
-        <h3 style={contentStyle}>Select Your candidate for each position</h3>
+        <Card title="Select Your candidate for each position" bordered={false} style={contentStyle}>
+          <p>After you have securely logged in, you will be able to proceed to the voting page. In this page, you will be provided with a list of candidates for each position. You are expected to pick your choice from among the available candidates. The choices you pick are to a maximum available for the position.</p>
+          <p></p>
+          <p></p>
+        </Card>
       </div>
       <div>
         <h3 style={contentStyle}>Submit your ballot</h3>
