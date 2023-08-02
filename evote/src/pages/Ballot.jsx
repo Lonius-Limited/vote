ffcb75bb9bd58ceeca7c4da0b908ef230354ab9a
saleemdev <dsmwaura@gmail.com>
2023-08-02 14:27:50 -0600

    Feature: added a timer for upcoming election

--- evote/src/pages/Ballot.jsx
+++ evote/src/pages/Ballot.jsx
@@ -4,13 +4,16 @@ import { _defaultHeaders } from "../api/queries";
 import { getCookie, setCookie } from "../lib/cookies";
 import NoBallot from "../components/NoBallot";
 import { useNavigate } from "react-router-dom";
+import { Statistic } from "antd";
+const { Countdown } = Statistic;
+
 const Ballot = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const voterRegistration = getCookie("voter_registration_details");
   const status = "just_any";
   const { voter_id } = JSON.parse(voterRegistration);
   // console.log("vid",voterRegistration,voter_id)
-  const params = { voter_id };
+  const params = { voter_id, status }; //status to be added , status
   const method = "vote.utils.election_details.get_voter_elections";
   const { data, error, isValidating } = useFrappeGetCall(
     method,
@@ -25,16 +28,39 @@ const Ballot = () => {
   }
   if (data) {
     const scheduledOROpen =
-      data.message.find((x) => ["Scheduled", "Open"].includes(x.status)) || {};
-      // data.message.find((x) => ["Closed"].includes(x.status)) || {};
-    if (Object.keys(scheduledOROpen).length < 1) {
+      data.message.filter((x) => ["Scheduled", "Open"].includes(x.status)) ||
+      [];
+    // data.message.find((x) => ["Closed"].includes(x.status)) || {};
+    if (scheduledOROpen.length < 1) {
       return <NoBallot />;
     }
-    setCookie("active_elections", JSON.stringify(scheduledOROpen), 12);
+    const open = scheduledOROpen.find((x) => ["Open"].includes(x.status));
+    const scheduled = scheduledOROpen.find((x) =>
+      ["Scheduled"].includes(x.status)
+    );
 
-    // navigate(`/ballot/${scheduledOROpen.name}`)
-    window.location.href = `/evote/ballot/${scheduledOROpen.name}`
+    if (scheduled) {
+      return <ScheduledElectionTimer election={scheduled} />;
+    }
+    if (open) {
+      setCookie("active_elections", JSON.stringify(scheduledOROpen), 12);
+
+      window.location.href = `/evote/ballot/${scheduledOROpen.name}`;
+    }
   }
 };
-
+const ScheduledElectionTimer = ({ election }) => {
+  const { election_start, name } = election;
+  const deadline = new Date(election_start);
+  return (
+    <>
+      {" "}
+      <Countdown
+        title={`There is an upcoming election on ${deadline.toDateString()}`}
+        value={deadline}
+        format="D day H hour m : s "
+      />
+    </>
+  );
+};
 export default Ballot;
