81975eab3637bd020332c43891ba8f69de6f2d61
saleemdev <dsmwaura@gmail.com>
2023-08-02 08:59:51 -0600

    Added a list to the elections page and improved logout experience

--- evote/src/pages/Results.jsx
+++ evote/src/pages/Results.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { useFrappeGetCall } from "frappe-react-sdk";
 import { _defaultHeaders } from "../api/queries";
 import { getCookie, setCookie } from "../lib/cookies";
-import NoBallot from "../components/NoBallot";
 import { useNavigate } from "react-router-dom";
+import { Button, Empty, List } from "antd";
+
 const Results = () => {
-  const navigate = useNavigate();
   const voterRegistration = getCookie("voter_registration_details");
   const status = "just_any";
   const { voter_id } = JSON.parse(voterRegistration);
   // console.log("vid",voterRegistration,voter_id)
-  const params = { voter_id };
+  const params = { voter_id, status };
   const method = "vote.utils.election_details.get_voter_elections";
   const { data, error, isValidating } = useFrappeGetCall(
     method,
@@ -24,17 +24,59 @@ const Results = () => {
     return <>{JSON.stringify(error)}</>;
   }
   if (data) {
-    const scheduledOROpen =
-      data.message.find((x) => ["Scheduled", "Open"].includes(x.status)) || {};
+    const closedOROpen =
+      data.message.filter((x) => ["Closed", "Open"].includes(x.status)) || [];
     // data.message.find((x) => ["Closed"].includes(x.status)) || {};
-    if (Object.keys(scheduledOROpen).length < 1) {
-      return <NoBallot />;
+    if (closedOROpen.length < 1) {
+      <Empty
+        image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+        imageStyle={{
+          height: 60,
+        }}
+        description={<span>No stats for now.</span>}
+      >
+        <Button type="primary"onClick={()=>window.location.href="/evote/logout"}>Log Me Out</Button>
+      </Empty>;
     }
-    setCookie("active_elections", JSON.stringify(scheduledOROpen), 12);
+    if (closedOROpen.length === 1) {
+      setCookie("active_elections", JSON.stringify(closedOROpen[0].name), 12);
+      window.location.href = `/evote/stats/${closedOROpen[0].name}`;
+      return;
+    }
+    return <RecentElections payload={closedOROpen} />;
+    //
 
     // navigate(`/ballot/${scheduledOROpen.name}`)
-    window.location.href = `/evote/stats/${scheduledOROpen.name}`;
+    // window.location.href = `/evote/stats/${scheduledOROpen.name}`;
   }
 };
+const RecentElections = ({ payload }) => {
+  return (
+    <>
+    <h2>Please select one of the elections below to view stats</h2>
+    <List
+      itemLayout="horizontal"
+      dataSource={payload}
+      renderItem={(item, index) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              <Avatar
+              style={{
+                backgroundColor: '#fde3cf',
+                color: '#f56a00',
+              }}
+              >...</Avatar>
+            }
+            title={<a href={`/evote/stats/${item.name}`}>{item.name}</a>}
+            description={`${item.election_start} - ${item.election_ends} | ${item.status}`}
 
+          />
+        </List.Item>
+        
+      )}
+    />
+    </>
+  );
+};
 export default Results;
